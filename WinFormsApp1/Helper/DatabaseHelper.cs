91a5f93c3c0eb2d212324b4a7f3b680c1d835667
Fathir <maulanafathir765@gmail.com>
2026-05-26 23:16:42 +0700

    commit

---
+++ WinFormsApp1/Helper/DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjek.DatabaseHelper
+{
+    public class DatabaseHelper
+    {
+        private static string connString = "Host=ep-wispy-river-aoxu1bki-pooler.c-2.ap-southeast-1.aws.neon.tech; Database=neondb; Username=neondb_owner; Password=npg_8sXeqj6NgdTE; SSL Mode=VerifyFull; Channel Binding=Require;";
+
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(connString);
+        }
+    }
+}
---
+++ WinFormsApp1/Interface/IProduct.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjek.Interface
+{
+    internal class IProduct
+    {
+    }
+}
