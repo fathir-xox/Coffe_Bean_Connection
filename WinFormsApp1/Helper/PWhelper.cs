368fd5f8afbb3c6ec65035787794791abc19d4c1
Fathir <maulanafathir765@gmail.com>
2026-05-28 00:48:49 +0700

    commit

--- WinFormsApp1/Helper/PWhelper.cs
+++ WinFormsApp1/Helper/PWhelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace FinalProjek.DatabaseHelper
+namespace FinalProjek.Helper
 {
     public static class PWhelper
     {
