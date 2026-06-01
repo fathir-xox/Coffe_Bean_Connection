92e9ff929839802c6e3ea72dc49c43c63cbf8b1a
racheljadma89-art <racheljadma89@gmail.com>
2026-06-01 10:27:36 +0700

    update nich

--- WinFormsApp1/Model/DetailTransaksi.cs
+++ WinFormsApp1/Model/DetailTransaksi.cs
@@ -6,23 +6,12 @@ namespace FinalProjek.Model
 {
     public class DetailTransaksi
     {
-        public int idDetailTransaksi { get; set; }
-        public int idTransaksi { get; set; }
-        public int idProduk { get; set; }
-        public string namaProduk { get; set; }
-        public decimal harga { get; set; }
-        public int qty { get; set; }
-        public decimal subtotal { get; set; }
-
-        public DetailTransaksi(int idDetailTransaksi, int idTransaksi, int idProduk, string namaProduk, decimal harga, int qty, decimal subtotal)
-        {
-            this.idDetailTransaksi = idDetailTransaksi;
-            this.idTransaksi = idTransaksi;
-            this.idProduk = idProduk;
-            this.namaProduk = namaProduk;
-            this.harga = harga;
-            this.qty = qty;
-            this.subtotal = harga * qty;
-        }
+        public int IdDetailTransaksi { get; set; }
+        public int IdTransaksi { get; set; }
+        public int IdProduk { get; set; }
+        public string NamaProduk { get; set; }
+        public int Harga { get; set; }
+        public int Qty { get; set; }
+        public int Subtotal { get; set; }
     }
 }
--- WinFormsApp1/Model/Transaksi.cs
+++ WinFormsApp1/Model/Transaksi.cs
@@ -6,25 +6,14 @@ namespace FinalProjek.Model
 {
     public class Transaksi
     {
-        public int idTransaksi { get; set; }
-        public int idKasir { get; set; }
-        public DateTime tanggal { get; set; }
-        public decimal totalHarga { get; set; }
-        public decimal jumlahBayar { get; set; }
-        public decimal kembalian { get; set; }
-        public string metodePembayaran { get; set; }
-        public string statusTransaksi { get; set; }
-
-        public Transaksi(int idTransaksi, int idKasir, DateTime tanggal, decimal totalHarga, decimal jumlahBayar, decimal kembalian, string metodePembayaran, string statusTransaksi)
-        {
-            this.idTransaksi = idTransaksi;
-            this.idKasir = idKasir;
-            this.tanggal = tanggal;
-            this.totalHarga = totalHarga;
-            this.jumlahBayar = jumlahBayar;
-            this.kembalian = kembalian;
-            this.metodePembayaran = metodePembayaran;
-            this.statusTransaksi = statusTransaksi;
-        }
+        public int IdTransaksi { get; set; }
+        public int IdKasir { get; set; }
+        public DateTime Tanggal { get; set; }
+        public int TotalHarga { get; set; }
+        public int JumlahBayar { get; set; }
+        public int Kembalian { get; set; }
+        public string MetodePembayaran { get; set; }
+        public string StatusTransaksi { get; set; }
+        public List<DetailTransaksi> Detail { get; set; }
     }
 }
