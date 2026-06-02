7e76e3925fe794925448e8c627efbe3cbb465769
Fathir <maulanafathir765@gmail.com>
2026-06-02 22:50:07 +0700

    commit

--- WinFormsApp1/View/Register.cs
+++ WinFormsApp1/View/Register.cs
@@ -13,10 +13,9 @@ namespace FinalProjek.View
         {
             InitializeComponent();
             _controller = new AuthController();
-<<<<<<< HEAD
-=======
+
             tbPasswordRegister.UseSystemPasswordChar = true;
->>>>>>> 2430eceb5a5b07e378109fe0e0b41e366520e1c2
+
         }
 
         private void label2_Click(object sender, EventArgs e)
