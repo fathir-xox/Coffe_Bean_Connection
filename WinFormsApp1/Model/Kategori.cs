b962d8ba3e1cf1840808db2287a0241bb35a17b2
racheljadma89-art <racheljadma89@gmail.com>
2026-06-09 22:57:22 +0700

    commit

---
+++ WinFormsApp1/Model/Kategori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace FinalProjek.Model
+{
+    public class Kategori
+    {
+        public int id_kategori { get; set; }
+        public string nama_kategori { get; set; }
+        public string deskripsi { get; set; }
+        public bool is_active { get; set; }
+    }
+}
