3602a861b15748e3402711b56cb2e20f0ea0b5d5
nasya10 <alvinnasya5@gmail.com>
2026-06-14 09:40:13 +0700

    commit

--- WinFormsApp1/View/Kasir_View/KasirDashboardView.Designer.cs
+++ WinFormsApp1/View/Kasir_View/KasirDashboardView.Designer.cs
@@ -29,16 +29,18 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            btTransaksi = new Button();
-            btRiwayatTransaksi = new Button();
-            btDaftarProduk = new Button();
             btLogout = new Button();
+            btDaftarProduk = new Button();
+            btRiwayatTransaksi = new Button();
+            btTransaksi = new Button();
+            panel2 = new Panel();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackgroundImage = Properties.Resources.VTransaksi;
+            panel1.Controls.Add(panel2);
             panel1.Controls.Add(btLogout);
             panel1.Controls.Add(btDaftarProduk);
             panel1.Controls.Add(btRiwayatTransaksi);
@@ -47,6 +49,42 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(1900, 1026);
             panel1.TabIndex = 0;
+            // 
+            // btLogout
+            // 
+            btLogout.BackColor = Color.Red;
+            btLogout.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btLogout.ForeColor = Color.White;
+            btLogout.Location = new Point(37, 912);
+            btLogout.Name = "btLogout";
+            btLogout.Size = new Size(224, 41);
+            btLogout.TabIndex = 11;
+            btLogout.Text = "Logout";
+            btLogout.UseVisualStyleBackColor = false;
+            // 
+            // btDaftarProduk
+            // 
+            btDaftarProduk.BackColor = Color.OldLace;
+            btDaftarProduk.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btDaftarProduk.ForeColor = Color.FromArgb(100, 60, 20);
+            btDaftarProduk.Location = new Point(77, 428);
+            btDaftarProduk.Name = "btDaftarProduk";
+            btDaftarProduk.Size = new Size(224, 41);
+            btDaftarProduk.TabIndex = 5;
+            btDaftarProduk.Text = "Daftar Produk";
+            btDaftarProduk.UseVisualStyleBackColor = false;
+            // 
+            // btRiwayatTransaksi
+            // 
+            btRiwayatTransaksi.BackColor = Color.OldLace;
+            btRiwayatTransaksi.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btRiwayatTransaksi.ForeColor = Color.FromArgb(100, 60, 20);
+            btRiwayatTransaksi.Location = new Point(77, 304);
+            btRiwayatTransaksi.Name = "btRiwayatTransaksi";
+            btRiwayatTransaksi.Size = new Size(224, 41);
+            btRiwayatTransaksi.TabIndex = 4;
+            btRiwayatTransaksi.Text = "Riwayat Transaksi";
+            btRiwayatTransaksi.UseVisualStyleBackColor = false;
             // 
             // btTransaksi
             // 
@@ -61,41 +99,14 @@
             btTransaksi.UseVisualStyleBackColor = false;
             btTransaksi.Click += btDashboar_Click;
             // 
-            // btRiwayatTransaksi
+            // panel2
             // 
-            btRiwayatTransaksi.BackColor = Color.OldLace;
-            btRiwayatTransaksi.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btRiwayatTransaksi.ForeColor = Color.FromArgb(100, 60, 20);
-            btRiwayatTransaksi.Location = new Point(77, 304);
-            btRiwayatTransaksi.Name = "btRiwayatTransaksi";
-            btRiwayatTransaksi.Size = new Size(224, 41);
-            btRiwayatTransaksi.TabIndex = 4;
-            btRiwayatTransaksi.Text = "Riwayat Transaksi";
-            btRiwayatTransaksi.UseVisualStyleBackColor = false;
-            // 
-            // btDaftarProduk
-            // 
-            btDaftarProduk.BackColor = Color.OldLace;
-            btDaftarProduk.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btDaftarProduk.ForeColor = Color.FromArgb(100, 60, 20);
-            btDaftarProduk.Location = new Point(77, 428);
-            btDaftarProduk.Name = "btDaftarProduk";
-            btDaftarProduk.Size = new Size(224, 41);
-            btDaftarProduk.TabIndex = 5;
-            btDaftarProduk.Text = "Daftar Produk";
-            btDaftarProduk.UseVisualStyleBackColor = false;
-            // 
-            // btLogout
-            // 
-            btLogout.BackColor = Color.Red;
-            btLogout.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btLogout.ForeColor = Color.White;
-            btLogout.Location = new Point(37, 912);
-            btLogout.Name = "btLogout";
-            btLogout.Size = new Size(224, 41);
-            btLogout.TabIndex = 11;
-            btLogout.Text = "Logout";
-            btLogout.UseVisualStyleBackColor = false;
+            panel2.BackgroundImage = Properties.Resources.CardTransaksi;
+            panel2.BackgroundImageLayout = ImageLayout.Zoom;
+            panel2.Location = new Point(1432, 170);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(453, 783);
+            panel2.TabIndex = 12;
             // 
             // KasirDashboardView
             // 
@@ -116,5 +127,6 @@
         private Button btRiwayatTransaksi;
         private Button btTransaksi;
         private Button btLogout;
+        private Panel panel2;
     }
 }
