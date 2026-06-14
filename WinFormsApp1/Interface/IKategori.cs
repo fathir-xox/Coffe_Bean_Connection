9cd1abdcad845da8cfb7938f93e741515b5b76db
nasya10 <alvinnasya5@gmail.com>
2026-06-14 11:12:16 +0700

    commit

---
+++ WinFormsApp1/Interface/IKategori.cs
@@ -0,0 +1,18 @@
+﻿using FinalProjek.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjek.Interface
+{
+    public interface IKategori
+    {
+        void CreateKategori(Kategori kategori);
+        List<Kategori> GetAllKategori();
+        List<Kategori> GetActiveKategori();
+        Kategori GetKategoriById(int id);
+        bool UpdateKategori(Kategori kategori);
+        bool DeleteKategori(object id);    // soft delete (set is_active = false)
+        bool RestoreKategori(int id);      // optional: set is_active = true
+    }
+}
