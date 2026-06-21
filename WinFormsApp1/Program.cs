0fa6c5396c227d73b63fa9e3e971da6aa82738f3
Fathir <maulanafathir765@gmail.com>
2026-06-21 14:44:11 +0700

    commit

---
+++ WinFormsApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using FinalProjek.View; // Pastikan ini ada agar sistem mengenali Form Login
+
+namespace FinalProjek
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+
+            Application.Run(new Login());
+        }
+    }
+}
