8c7a602ff37fac10ba6c9aaecaf38699e76b9281
Fathir <maulanafathir765@gmail.com>
2026-06-21 13:21:23 +0700

    commit

---
+++ WinFormsApp1/Model/Kategori.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace FinalProjek.Model
+{
+    public class Kategori : BaseModel
+    {
+        public int id_kategori { get; set; }
+        public string nama_kategori { get; set; }
+        public string deskripsi { get; set; }
+    }
+}
---
+++ WinFormsApp1/Model/Produk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjek.Model
+{
+    public class Produk : BaseModel
+    {
+        public int id_produk { get; set; }
+        public int id_kategori { get; set; }
+        public string? nama_produk { get; set; }
+        public int harga { get; set; }
+        public int stok { get; set; }
+        public string? deskripsi { get; set; }
+        public byte[]? imageproduk { get; set; }
+    }
+}
